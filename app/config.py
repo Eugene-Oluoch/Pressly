e428fb11445cb27b2d800efe129850065b9e0733
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-03-13 14:53:31 +0300

    Changed the port in the DATABASE URL

--- app/config.py
+++ app/config.py
@@ -12,7 +12,7 @@ class Config:
 class ProdConfig(Config):
     
     #Development Database Configuration 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://crabs:Greenland@localhost/pressly'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://crabs:Greenland@localhost/5433/pressly'
 
 
 class DevConfig(Config):
